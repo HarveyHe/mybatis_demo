33e6b70aebfd52889fcfc2212d76746c884b3d24
Harvey.He <Harvey.He@gsstharvey.gsst.com>
2018-03-02 17:55:03 +0800

    mybatis实战教程(mybatis in action)之三:实现数据的增删改查

--- src/main/java/com/yihaomen/mybatis/model/UserMapper.java
+++ src/main/java/com/yihaomen/mybatis/model/UserMapper.java
@@ -1,5 +1,17 @@
 package com.yihaomen.mybatis.model;
 
+import java.util.List;
+
 public interface UserMapper {
 	public User selectUserByID(int id);
+	
+	public List<User> selectUsers(String userName);
+	
+	
+	public void addUser(User user);
+	
+	public void updateUser(User user);
+	
+	public void deleteUser(int id);
+	
 }
